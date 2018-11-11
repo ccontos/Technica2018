522f2097a4dc1a82c803082e9aaf8c3a938cc904
Chloe Contos <ccontos@umail.iu.edu>
2018-11-10 23:48:15 -0500

    Adding Tips Activity

--- app/src/main/java/com/example/chloecontos/technica2018/PreparingSpendingPlan.java
+++ app/src/main/java/com/example/chloecontos/technica2018/PreparingSpendingPlan.java
@@ -1,13 +1,28 @@
 package com.example.chloecontos.technica2018;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PreparingSpendingPlan extends AppCompatActivity {
+
+    private static final boolean USE_FLAG = true;
+    private static final int myFlag = Intent.FLAG_ACTIVITY_REORDER_TO_FRONT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preparing_spending_plan);
+
+        final Button button = findViewById(R.id.tipButton);
+    }
+
+    public void tipOnClick(View view){
+        Intent preparingspendingplantips = new Intent(PreparingSpendingPlan.this, PreppingSpendingPlanTips.class);
+        if(USE_FLAG)
+            preparingspendingplantips.addFlags(myFlag);
+        startActivity(preparingspendingplantips);
     }
 }
